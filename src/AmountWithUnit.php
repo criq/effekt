5eda27de35a325df6c6b623d20256beb826cb6ee
Jan Jablunka <jablunka@gmail.com>
2017-09-29 15:36:37 +0200

    incremental

---
+++ src/AmountWithUnit.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Effekt;
+
+class AmountWithUnit {
+
+	public $amount;
+	public $unit;
+
+	public function __construct($amount, $unit) {
+		$this->amount = static::convertToFloat($amount);
+		$this->unit = trim($unit);
+	}
+
+	static function convertToFloat($string) {
+		return (float) floatval(trim(strtr(preg_replace('/\s/u', null, $string), ',', '.')));
+	}
+
+}
